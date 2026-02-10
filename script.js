f8a0dcabf8f946c6a8cc27d1fd217f1e83673c5e
DiegoVcRb <182699286+DiegoVcRb@users.noreply.github.com>
2026-02-10 16:46:42 -0600

    Se actualizo la fincionalidad de actualizar los contenedores

--- script.js
+++ script.js
@@ -1,48 +1,124 @@
-/* Referencias al documento del DOM */
+/*  Referencias al documento DOM  */
 
-const tareaEntrada = document.getElementById ('tareaEntrada');
-const botonAgregar = document.getElementById (botonAgregar);
-const contenedorTareas = document.getElementById (contenedorTareas);
+const tareaEntrada = document.getElementById("tareaEntrada");
+const botonAgregar = document.getElementById("botonAgregar");
+const contenedorTareas = document.getElementById("contenedorTareas");
+const mensaje = document.getElementById("mensaje");
+const contadorTotales = document.getElementById("contadorTotales");
+const contadorTerminadas = document.getElementById("contadorTerminadas");
 
-/* Función para crear el elemento tarea (función creadora de Nodo Tarea) */
+/* Función para crear el elemnto tarea (Función creadora del Nodo Tarea) */
 
-function crearElementoTarea (){
-  // Crear elemento de la tarea
-  const tareaContenedor = document.createElement('div');
-  const tareaTexto = document.createElement('p');
-  const iconosContenedor = document.createElement('div');
-  const iconoCompletada = document.createElement('i');
-  const iconoEliminar = document.createElement('i');
+function crearElementoTarea(){
 
-  // Creamos la estructura de la tarea
-  iconosContenedor.append(iconoCompletada, iconoEliminar);
-  tareaContenedor.append(tareaTexto, iconosContenedor);
+    // Crear los elemntos html de la tarea
+    const tareaContenedor = document.createElement('div');
+    const tareaTexto = document.createElement('p');
+    const iconoContenedor = document.createElement('div');
+    const iconoCompletada = document.createElement('i');
+    const iconoEliminar = document.createElement('i');
 
-  // Agregamos las clases a los elementos de la tarea
-  tareaContenedor.classList.add('tarea');
-  tareaTexto.classList.add('tarea-texto');
-  iconosContenedor.classList.add('tarea-iconos');
-  iconoCompletada.classList.add('bi', 'bi-dash-circle');
-  iconoEliminar.classList.add('bi', 'bi-trash2');
+    /* Creamos la estructura de la tarea */
 
+    iconoContenedor.append(iconoCompletada , iconoEliminar);
+    tareaContenedor.append(tareaTexto , iconoContenedor);
 
-  // Agregamos el texto de usuario
-  tareaTexto.innerText = tareaEntrada.value;
+    /* Agregamos las clases a los contenedores de la tarea */
 
-  // Retomammos la estructura de la tarea 
-  return tareaContenedor; 
+    tareaContenedor.classList.add('tarea');
+    tareaTexto.classList.add('tarea-texto');
+    iconoContenedor.classList.add('tarea-iconos');
+    iconoCompletada.classList.add('bis' , 'bi-check-circle');
+    iconoEliminar.classList.add('bis' , 'bi-trash2');
+
+    /* Agregamos el texto del usuario */
+    tareaTexto.innerText = tareaEntrada.value;
+
+    /* Escuchadores de los Iconos */
+    iconoCompletada.addEventListener('click', (e) => {
+
+        const tareaElemento = e.target.parentNode.parentNode
+        const esCompletada = tareaElemento.classList.contains('tarea-completada')
+
+        tareaElemento.classList.toggle('tarea-completada');
+        if(esCompletada){
+            e.target.classList.remove('bi-dash-circle')
+            e.target.classList.add('bi-check-circle');
+        }else{
+            e.target.classList.add('bi-dash-circle');
+            e.target.classList.remove('bi-check-circle')
+        }
+
+        //  Actualizamos los contadores
+        actualizarContadores();
+
+    })
+
+    iconoEliminar.addEventListener('click', (e) => {
+        const tareaElemento = e.target.parentNode.parentNode;
+        tareaElemento.remove();
+        //  Actualizamos los contadores
+        actualizarContadores();
+    })
+
+    /* Retornamos la estructura de la tarea */
+    return tareaContenedor;
+
+}
+
+/*  Función Actualizar Contadores  */
+
+function actualizarContadores(){
+    // Contamos los elementos con la clase tarea
+    const tareasTotales = document.querySelectorAll('.tarea');
+    const tareasCompletadas = document.querySelectorAll('.tarea-completada');
+
+    // Actualizamos los contadores en el DOM
+   contadorTotales.textContent = tareasTotales.length;
+   contadorTerminadas.textContent = tareasCompletadas.length;
 }
 
 /* Escuchador */
-botonAgregar.addEventListener('click',crearElementoTarea);
 
-// Funcion para agregar El elemento tarea
+botonAgregar.addEventListener("click" , agregarTarea);
 
-function agregarTareaa() {
-  // Traemos el elemnto retornado por la funcion crearElementoTarea
-  const elementoTarea = crearElementoTarea();
-  contenedorTareas.append(elementoTarea);
+/* Función Agregar el Elemento Tarea */
 
-  // Reiniciar el valor del input
-  tareaEntrada.value = '';
-}
+    function agregarTarea(){
+
+        //Consante para evaluar si hay texto o no.
+        const texto = tareaEntrada.value.trim();
+
+        if(!texto) {
+            mensaje.textContent = 'Agrega texto! 💢';
+        }else{
+             // Traemos el elemento retornado por la función crearElementoTarea
+             const elementoTarea = crearElementoTarea();
+             contenedorTareas.append(elementoTarea);
+
+             // Reiniciar el value del input
+             tareaEntrada.value = ' ';
+
+             mensaje.textContent = 'Tarea creada correctamente! 👍';
+
+             //  Actualizamos los contadores
+             actualizarContadores();
+        }
+
+    }
+
+/*  Hacemos que al presionar enter en el input se agregue la tarea */
+
+document.addEventListener('keydown', (e) => {
+    if(e.key == "Enter") {
+        agregarTarea();
+    }
+})
+
+tareaEntrada.addEventListener('input', () => {
+    if(tareaEntrada.value.trim() === ""){
+        mensaje.textContent = 'Escribe tu próxima tarea c:'
+    }else{
+        mensaje.textContent = 'Presiona enter! c:'
+    }
+})
